83a17cb702f5063c5e219bc80c7494e26c4a5646
niubrandon <niubrandon@gmail.com>
2021-09-13 15:44:33 -0400

    added an asserEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(2, 4);
+assertEqual(2, true);
+assertEqual(2, []);
---
+++ sum.js
@@ -0,0 +1,6 @@
+const sum = function(a,b) {
+  return a + b;
+};
+
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3);
