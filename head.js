b878d084f8e5fabee2befb843da8d2fedd8c620d
niubrandon <niubrandon@gmail.com>
2021-09-13 16:04:54 -0400

    created a head function

---
+++ head.js
@@ -0,0 +1,24 @@
+const head = (arr) => {
+  if (arr === []) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+  // console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+  // console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([5]), 5);
+assertEqual(head(["Hello"]), "Hello");
