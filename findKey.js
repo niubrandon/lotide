bffeea12b9e228e97a176d28cbf8ca9727960ec3
niubrandon <niubrandon@gmail.com>
2021-09-16 15:55:25 -0400

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+  // console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+  // console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  //looping through object key
+  for (let key of Object.keys(object)) {
+  // console.log(key);
+    console.log(object[key]["stars"]);
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma");
