479a9af30211fb77f99437df5af2965a1f5504b1
niubrandon <niubrandon@gmail.com>
2021-09-17 20:00:21 -0400

    updated

--- eqArrays.js
+++ eqArrays.js
@@ -10,17 +10,42 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = (arrA, arrB) => {
   let isEqual = true;
+  //first, check if both array has same length
   if (arrA.length !== arrB.length) {
-    isEqual = false;
+  // console.log("two array has different length", arrA, arrB);
+    return isEqual = false;
   }
+  //loop throught each element
   for (let i = 0; i < arrA.length; i++) {
-    if (arrA[i] !== arrB[i]) {
-      isEqual = false;
+  //check if element is array
+  // console.log("looping thourgh", i , arrA[i], arrB[i]);
+    if (Array.isArray(arrA[i]) && Array.isArray(arrB[i])) {
+    //recursive call
+      console.log("comparing two arrays", arrA[i], arrB[i]);
+      isEqual = eqArrays(arrA[i], arrB[i]);
+
+    } else if ((!Array.isArray(arrA[i]) && Array.isArray(arrB[i])) || (Array.isArray(arrA[i]) && !Array.isArray(arrB[i]))) {
+    // console.log("one of them is not array", arrA[i], arrB[i]);
+      return isEqual = false;
+      
+    } else {
+      if (arrA[i] !== arrB[i]) {
+      // console.log("two value are not same", arrA[i], arrB[i]);
+        return isEqual = false;
+      }
     }
   }
   return isEqual;
 };
 
+eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); 
+eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+ 
+eqArrays([[2, 3], [4]], [[2, 3], 4]); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+ 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
@@ -29,5 +54,4 @@ assertEqual(eqArrays([0], [1]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays(["hi", 2, 3], ["hi", 2, 3]), true);
 assertEqual(eqArrays([1, 2, "hi"], [1, 2, 3]), false);
-console.log(eqArrays([1, 2, 3], [1, 2, 3]),eqArrays([1, 2, 3], [3, 2, 1]),eqArrays(["1", "2", "3"], ["1", "2", "3"]),eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
+ 
--- eqObjects.js
+++ eqObjects.js
@@ -10,12 +10,29 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = (arrA, arrB) => {
   let isEqual = true;
+  //first, check if both array has same length
   if (arrA.length !== arrB.length) {
-    isEqual = false;
+  // console.log("two array has different length", arrA, arrB);
+    return isEqual = false;
   }
+  //loop throught each element
   for (let i = 0; i < arrA.length; i++) {
-    if (arrA[i] !== arrB[i]) {
-      isEqual = false;
+  //check if element is array
+  // console.log("looping thourgh", i , arrA[i], arrB[i]);
+    if (Array.isArray(arrA[i]) && Array.isArray(arrB[i])) {
+    //recursive call
+      console.log("comparing two arrays", arrA[i], arrB[i]);
+      isEqual = eqArrays(arrA[i], arrB[i]);
+
+    } else if ((!Array.isArray(arrA[i]) && Array.isArray(arrB[i])) || (Array.isArray(arrA[i]) && !Array.isArray(arrB[i]))) {
+    // console.log("one of them is not array", arrA[i], arrB[i]);
+      return isEqual = false;
+      
+    } else {
+      if (arrA[i] !== arrB[i]) {
+      // console.log("two value are not same", arrA[i], arrB[i]);
+        return isEqual = false;
+      }
     }
   }
   return isEqual;
@@ -41,7 +58,7 @@ const eqObjects = function(object1, object2) {
       } else {
         //both are objects, use recursion to check, pass new object to eqObjects
         return eqObjects(object1[item], object2[item]);
-            
+
       }
     } else {
       // then compare if both them are not object
