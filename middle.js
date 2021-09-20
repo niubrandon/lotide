582e84e5c009fa9593b1feb0920160cfe36d5044
niubrandon <niubrandon@gmail.com>
2021-09-20 14:35:52 -0400

    adding mocha and chai

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = (arrA, arrB) => {
-  let isEqual = true;
-  if (arrA.length !== arrB.length) {
-    isEqual = false;
-  }
-  for (let i = 0; i < arrA.length; i++) {
-    if (arrA[i] !== arrB[i]) {
-      isEqual = false;
-    }
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = (arrA, arrB) => {
-  
-  let isEqual = eqArrays(arrA, arrB);
-  
-  return (isEqual ? console.log(`✅✅✅Assertion Passed: ${arrA} === ${arrB}`) : console.log(`🛑🛑🛑Assertion Failed: ${arrA} !== ${arrB}`));
-};
-
 const middle = (arr) => {
   let length = arr.length;
   let mid = Math.floor(arr.length / 2) - 1;
@@ -35,9 +15,4 @@ const middle = (arr) => {
 
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
